2906bb31a0d13c56eecf23083029735e698987a9
minerguy31 <minerguy31@gmail.com>
2016-09-26 17:55:03 -0600

    Add genericToString

--- src/jpkg/arraygenerics/ArrayGenerics.java
+++ src/jpkg/arraygenerics/ArrayGenerics.java
@@ -1,11 +1,55 @@
 package jpkg.arraygenerics;
 
+import java.util.Arrays;
+
+/**
+ * A set of utilities for dealing with primitive arrays.
+ * @author minerguy31
+ *
+ */
 public final class ArrayGenerics {
+	/**
+	 * Checks if a given object is an array
+	 * @param obj
+	 * @return
+	 */
 	public static final boolean isArray(final Object obj) {
 	    return obj instanceof Object[] || obj instanceof boolean[] ||
 	       obj instanceof byte[] || obj instanceof short[] ||
 	       obj instanceof char[] || obj instanceof int[] ||
 	       obj instanceof long[] || obj instanceof float[] ||
 	       obj instanceof double[];
+	}
+	
+	/**
+	 * Get the textual representation for an object
+	 * 
+	 * If the passed object is an array, then the corresponding Arrays.toString method is called. 
+	 * Otherwise, the objects' own toString is called.
+	 * @param obj
+	 * @return
+	 */
+	public static final String genericToString(Object obj) {
+		if(obj == null)
+			return "null";
+		else if(obj instanceof Object[])
+			return Arrays.deepToString((Object[]) obj);
+		else if(obj instanceof boolean[])
+			return Arrays.toString((boolean[]) obj);
+		else if(obj instanceof byte[])
+			return Arrays.toString((byte[]) obj);
+		else if(obj instanceof short[])
+			return Arrays.toString((short[]) obj);
+		else if(obj instanceof char[])
+			return Arrays.toString((char[]) obj);
+		else if(obj instanceof int[])
+			return Arrays.toString((int[]) obj);
+		else if(obj instanceof long[])
+			return Arrays.toString((long[]) obj);
+		else if(obj instanceof float[])
+			return Arrays.toString((float[]) obj);
+		else if(obj instanceof double[])
+			return Arrays.toString((double[]) obj);
+		else return obj.toString();
 	}
 }
--- src/jpkg/arraygenerics/ArrayGenericsTest.java
+++ src/jpkg/arraygenerics/ArrayGenericsTest.java
@@ -2,6 +2,7 @@ package jpkg.arraygenerics;
 
 import static org.junit.Assert.*;
 import static jpkg.arraygenerics.ArrayGenerics.isArray;
+import static jpkg.arraygenerics.ArrayGenerics.genericToString;
 import jpkg.test.TestManager;
 
 import org.junit.Test;
@@ -17,12 +18,22 @@ public class ArrayGenericsTest {
 	}
 	
 	@Test
-	public void testObj() {
+	public void testIsArray() {
 		assertFalse(isArray(null));
 		assertFalse(isArray(1));
 		assertFalse(isArray("test"));
 		assertTrue(isArray(new int[] {}));
+		assertTrue(isArray(new int[][] {}));
+		assertTrue(isArray(new Object[][] {}));
 		assertTrue(isArray(new String[] {}));
 		assertTrue(isArray(new Object[] {null, null}));
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals("null", genericToString(null));
+		Object o = new Object();
+		assertEquals(o.toString(), genericToString(o));
+		assertEquals("[[1, 2], [3, 4]]", genericToString(new int[][] {{1, 2}, {3, 4}}));
 	}
 }
